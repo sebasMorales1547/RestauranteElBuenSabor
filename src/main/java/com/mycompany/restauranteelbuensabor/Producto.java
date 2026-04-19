b84ec3c9553f42d066587fc5dbaa9f919e88e51f
Sebastian Morales <sebasandres.1547@gmail.com>
2026-04-19 17:37:21 -0500

    feat: aplicar encapsulamiento a la clase Producto

--- src/main/java/com/mycompany/restauranteelbuensabor/Producto.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Producto.java
@@ -2,13 +2,38 @@ package com.mycompany.restauranteelbuensabor;
 
 public class Producto {
 
-    String nombre;
-    double precio;
-    int cantidad;
+    private String nombre;
+    private double precio;
+    private int cantidad;
 
     public Producto(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio;
         this.cantidad = 0;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
 }
